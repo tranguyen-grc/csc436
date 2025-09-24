cd6404348dfab75c7c3a39c263c5458562e37dfd
tranguyen-grc <chiptra292@gmail.com>
2025-09-24 10:04:07 -0700

    finished grad app

--- app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -16,8 +16,12 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,6 +33,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zybooks.graduationrsvp.ui.theme.GraduationRSVPTheme
+import androidx.compose.runtime.getValue
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,21 +70,47 @@ fun GradScreen(modifier: Modifier = Modifier) {
         )
         Image(
             painter = painterResource(R.drawable.grad_cap),
-            contentDescription = null,
+            contentDescription = stringResource(R.string.graduation_cap),
             alpha = 0.3F
         )
         Row(verticalAlignment = Alignment.CenterVertically) {
             Icon(
                 imageVector = Icons.Filled.Info,
-                contentDescription = "Information",
+                contentDescription = stringResource(R.string.information),
                 tint = Color.Gray,
-                modifier = Modifier.padding(10.dp).size(40.dp)
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(40.dp)
             )
             Text(
-                text = "May 14 - Ganus Hall - 2:00 pm",
+                text = stringResource(R.string.may_14_ganus_hall_2_00_pm),
                 fontSize = 20.sp
             )
         }
+        Rsvp(modifier)
+    }
+}
+
+@Preview
+@Composable
+fun Rsvp(modifier: Modifier = Modifier) {
+    var isChecked by remember { mutableStateOf(true) }
+    val message = if (isChecked) "I'll be there!" else "Sorry I can't be there."
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Switch(
+            checked = isChecked,
+            onCheckedChange = {
+                isChecked = it
+            }
+        )
+        Text(
+            text = message,
+            fontSize = 30.sp
+        )
     }
 }
 
